bf8122c76a9fce90d1d85e5a9ac09e78734dce27
emirbaki <emirbaki.068@gmail.com>
2025-07-18 17:57:00 +0300

    .

--- src/twitter/twitter.service.ts
+++ src/twitter/twitter.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CursoredData, Rettiwt, Tweet } from 'rettiwt-api';
+import * as dotenv from 'dotenv';
 
-const API_KEY =
-  'a2R0PW5nUEhzam1TSjFKUkRXTVRjNU12cjB6SG9XVm9ESmNKMWZFZlA1V0U7YXV0aF90b2tlbj00MjVlMTE4MDg0NDhlOWFiNDhmM2Q1Y2RmYjZmYThiZTU5Njg3MzQyO2N0MD01ZDI4ZGJmNGEwYzY1YWE5ZTc0MWE2OTI3NjZiMWUxMDdiNDMzZDBmM2FhMmQzNjljYjdkM2I4ZWZlNzBkYTJiMjczYTViYzk0OGNmZDMyZDA5NjFmNTA0NDkxOGU4Zjc2YjkxODAzYzhkMTg5NTYwMjIwNGVmMmNiYTUxNTllYmM2NzgxMWViZjQwZDYxNWQyMWVlYjI0MTNiNWZmM2VjO3R3aWQ9dSUzRDIyMDE3NzM5NDY7';
+try {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  dotenv.config();
+} catch (error) {
+  console.error('Error loading .env file:', error);
+}
+
+const API_KEY: string = process.env.TWITTER_API_KEY || '';
 const rettiwt = new Rettiwt({ apiKey: API_KEY });
 
 @Injectable()
@@ -28,7 +35,7 @@ export class TwitterService {
       .search(
         {
           fromUsers: ['yagosabuncuoglu'],
-          includeWords: ['osimhen'],
+          includeWords: [keyword],
         },
         20,
         data1.next.valueOf(),
